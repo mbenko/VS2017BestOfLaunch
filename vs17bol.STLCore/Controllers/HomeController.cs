8e899fc68540e18df02687037c29a60f5a07508b
Mike Benkovich <mike@benko.com>
2017-04-18 09:37:07 -0500

    Add project files.

---
+++ vs17bol.STLCore/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace vs17bol.STLCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            var MyClient = new HttpClient();
+            var rc = MyClient.GetAsync(MySettings.API_URL + "/api/values");
+
+            ViewData["Message"] = rc.Result;
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ vs17bol.STLCore/MySettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vs17bol.STLCore
+{
+    public class MySettings
+    {
+        public static string API_URL { get; set; }
+    }
+}
